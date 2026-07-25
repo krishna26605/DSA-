d89148da5071cfd07df251d183edf91366bd8718
Krishna Nikam <151912362+krishna26605@users.noreply.github.com>
2026-07-25 22:54:15 +0530

    Time: 3 ms (73.86%), Space: 16.7 MB (5.2%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,19 +1,145 @@
 class Solution {
+
+
+
+
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        
-        long long sum=0;
-        for(int i=0 ; i<n ; i++){
-            int smallest=nums[i];
-            int largest=nums[i];
-            for(int j=i+1 ; j<n;j++){
-                smallest=min(smallest,nums[j]);
-                largest=max(largest,nums[j]);
-                sum = sum + (largest-smallest);
-            }
+
+vector<int> FindNSE(vector<int> arr){
+    int n = arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+
+    for(int i=n-1; i>=0 ; i-- ){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=n;
+        }
+        else{
+            ans[i]=st.top();
         }
 
-        return sum;
+        st.push(i);
+    }
+
+    return ans;
+}
+
+vector<int>FindPSE(vector<int>arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+
+    for(int i=0 ; i<n ; i++){
+        while(!st.empty() && arr[st.top()]>arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=st.top();
+        }
+
+        st.push(i);
+    }
+
+    return ans;
+}
+
+vector<int>NGE(vector<int>arr){
+    int n = arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+
+    for(int i=n-1; i>=0;i--){
+
+        while(!st.empty() && arr[st.top()]<=arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=n;
+        }
+        else{
+            ans[i]=st.top();
+        }
+        st.push(i);
+    }
+
+    return ans;
+}
+
+vector<int>PGE(vector<int>arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+
+    for(int i=0 ; i<n ; i++){
+        while(!st.empty() && arr[st.top()]<arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=st.top();
+        }
+        st.push(i);
+    }
+    return ans;
+
+}
+
+long long subarrayMax(vector<int> & arr){
+    vector<int>pge=PGE(arr);
+    vector<int>nge=NGE(arr);
+
+    int n=arr.size();
+    long long  sum=0;
+
+    for(int i=0 ; i<n ; i++){
+        int left = i-pge[i];
+        int right=nge[i]-i;
+
+        long long freq= left*right*1LL;
+        long long contri = freq*arr[i]*1LL;
+
+        sum = sum + contri;
+    }
+
+    return sum;
+}
+
+long long subarrayMin(vector<int> & arr){
+
+    vector<int>pse= FindPSE(arr);
+    vector<int>nse= FindNSE(arr);
+
+    int n = arr.size();
+    long long  sum=0;
+
+    for(int i=0 ;i<n ; i++){
+
+        int left = i-pse[i];
+        int right= nse[i]-i;
+
+        long long freq = left*right*1LL;
+
+        long long contri = arr[i]*freq*1LL;
+
+        sum = sum + contri;
+    } 
+    
+    return sum;
+}
+
+
+
+    long long subArrayRanges(vector<int>& nums) {
+        
+        return ( subarrayMax(nums) - subarrayMin(nums) );
+
     }
 };
