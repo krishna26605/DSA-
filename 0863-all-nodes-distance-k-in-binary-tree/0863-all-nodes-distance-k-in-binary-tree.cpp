801d679db40bab3acb4b689e5c51d4dd8fb8abf3
Krishna Nikam <151912362+krishna26605@users.noreply.github.com>
2026-08-16 14:05:36 +0530

    Time: 4 ms (65.49%), Space: 15.4 MB (49.59%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    //BUILDING PARENT MAP
+    void mapParentNodes(TreeNode*root , unordered_map<TreeNode* , TreeNode*>&parentMap){
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+
+            TreeNode*node=q.front();
+            q.pop();
+
+            if(node->left){
+                parentMap[node->left]=node;
+                q.push(node->left);
+            }
+            if(node->right){
+                parentMap[node->right]=node;
+                q.push(node->right);
+            }
+        }
+    }
+
+    // FINDIN NODES AT DISTANCE K
+    vector<int>bfsAtDistanceK(TreeNode*target , unordered_map<TreeNode* , TreeNode*>&parentMap ,int k ){
+        queue<TreeNode*>q;
+        unordered_set<TreeNode*>visited;
+        q.push(target);
+        visited.insert(target);
+        int currentLevel=0;
+        
+
+        while(!q.empty()){
+            int size= q.size();
+
+            if(currentLevel++==k) break;
+
+            for(int i=0 ; i<size ; i++){
+                TreeNode*node=q.front();
+                q.pop();
+            
+
+            if(node->left && visited.find(node->left)== visited.end()){
+                visited.insert(node->left);
+                q.push(node->left);
+            }
+            if(node->right && visited.find(node->right)== visited.end()){
+                visited.insert(node->right);
+                q.push(node->right);
+            }
+
+            if(parentMap.count(node) && visited.find(parentMap[node])==visited.end()){
+                visited.insert(parentMap[node]);
+                q.push(parentMap[node]);
+            }
+            }
+
+        }
+
+        vector<int>result;
+        while(!q.empty()){
+            result.push_back(q.front()->val);
+            q.pop();
+        }
+
+        return result;
+    }
+
+
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode* , TreeNode*>parentMap;
+
+        mapParentNodes(root , parentMap);
+
+        return bfsAtDistanceK(target,parentMap,k);
+    }
+};
